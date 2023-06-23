3bd164d1ac55293b07648bdc5e7d637c8655eb45
Yasin <Yasin@saricam.com>
2023-06-23 15:13:30 +0300

    Version 1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,12 @@
+public class Class01 {
+
+    public static void main(String[] args) {
+        
+        int x = 1;
+        int y = 2;
+        int toplam = x + y ;
+        System.out.println("toplam = " + toplam);
+        System.out.println("Version 1 için işlemler tamam!.");
+    }
+
+}
